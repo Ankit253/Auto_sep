0bf18d788efaf46144b0033d9831e960548e363a
Ankit253 <yadavankit.ay253@gmail.com>
2021-06-19 01:23:05 +0530

    updating data

--- Unloading_parcels.py
+++ Unloading_parcels.py
@@ -1,17 +1,25 @@
+# robonox'20 team voltage
+# Code of unloading part
+
 import RPi.GPIO
 from time import sleep
 
-motor1 = 4
-motor2 = 14
+# motor 1
+m1 = 4
+#  Motor2
+m2 = 14  
 
+# main setup
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(motor1, GPIO.out)
     GPIO.setup(motor2, GPIO.out)
 
+# Working of motor 
 def loop():
     GPIO.output(motor1, HIGH)
     GPIO.output(motor2, LOW)
+
 
 def forward():
     setup()
